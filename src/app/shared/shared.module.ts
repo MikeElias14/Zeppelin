c898c960a9d10cec3efc9ff83932367242e4adba
MikeElias14 <michael@site469.com>
2019-05-28 19:45:38 -0400

    Finish cutting fat from Snarky

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSliderModule } from '@angular/material/';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -49,21 +40,14 @@ library.add(
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     MatButtonModule,
     MatToolbarModule,
     MatSelectModule,
-    MatTabsModule,
-    MatInputModule,
     MatProgressSpinnerModule,
-    MatChipsModule,
-    MatCardModule,
     MatSidenavModule,
-    MatCheckboxModule,
     MatListModule,
     MatMenuModule,
     MatIconModule,
-    MatTooltipModule,
     MatSnackBarModule,
     MatSlideToggleModule,
     MatDividerModule,
@@ -72,28 +56,18 @@ library.add(
   declarations: [],
   exports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     MatButtonModule,
     MatMenuModule,
-    MatTabsModule,
-    MatChipsModule,
-    MatInputModule,
     MatProgressSpinnerModule,
-    MatCheckboxModule,
-    MatCardModule,
     MatSidenavModule,
     MatListModule,
     MatSelectModule,
     MatToolbarModule,
     MatIconModule,
-    MatTooltipModule,
     MatSnackBarModule,
     MatSlideToggleModule,
     MatDividerModule,
     MatSliderModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     FontAwesomeModule,
   ]
 })
